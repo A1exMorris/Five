bc237ce03bd9839096c6a92edd710d1503139e05
A1exMorris <wertava@gmail.com>
2018-05-07 17:16:59 +0300

    NoteBook
    NoteBookTest Sync

---
+++ src/index.php
@@ -0,0 +1,95 @@
+<!doctype html>
+<html lang="en">
+  <head>
+      
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
+
+    <title>Starter Template for Bootstrap</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="assets/css/style.css" rel="stylesheet">
+<!--    <link href="assets/css/custom.css" rel="stylesheet">-->
+    
+      
+    <!-- Custom styles for this template -->
+    
+  </head>
+
+  <body>
+
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+<!--      <a class="navbar-brand h1" href="#">NP</a>
+-->      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse justify-content-center" id="navbarsExampleDefault">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Обо мне<span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Портфолио</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">Творчество</a>
+            </li>
+            <li class="nav-item">
+                <a href="#" class="nav-link">Контакты</a>
+            </li>
+        </ul>
+
+      </div>
+    </nav>
+
+
+
+
+ <!--Navbar-->
+
+    <div class="row jumbotron paral paralsec1">
+        <div class="col-12 col-sm-6 order-last order-sm-1 order-md-1 order-lg-1 order-xl-1" >
+            <div class="row h-100 align-items-end">
+                <div class="col-12 text-left text-center">
+                    <div class=" h4" style="color: whitesmoke"> Фотография это просто! Первые 5 снимков в первый день! </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 order-first order-sm-2 order-md-2 order-lg-2 order-xl-2" >
+            <div class="row h-100 align-items-center">
+                <div class="col-12 text-right pr-5">
+                    <div class="h1 fadeInTitle"> NPhotography </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <div class="container">
+        <p class="h1">dddddddddddd</p>
+
+    </div>
+
+    <!--Main part-->
+
+
+
+
+    <div class="footer text-center">
+        <div class="container">
+        <div class="row" >
+            <div class="col-12">
+                <span class="text-muted">AlexMorris</span>
+            </div>
+        </div>
+    </div>
+    </div><!--Footer-->
+    <!--Script section-->
+    <script src="assets/js/main.js"></script>
+  </body>
+
+</html>
