31b90308415ee6649008086a06e74b9027685ac0
A1exMorris <wertava@gmail.com>
2018-05-08 21:02:48 +0300

    Backgrounfd white + some change

--- build/index.php
+++ build/index.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-
+<!--    <link rel="icon" href="../../../../favicon.ico">
+-->
     <title>Starter Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
@@ -21,9 +21,9 @@
 
   <body>
 
-    <nav class="navbar navbar-expand-sm navbar-dark bg-dark fixed-top">
+    <nav class="navbar navbar-expand-md navbar-light bg-secondary fixed-top h1">
 <!--      <a class="navbar-brand h1" href="#">NP</a>
--->      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+-->      <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
 
@@ -55,7 +55,7 @@
         <div class="col-12 col-sm-6 order-last order-sm-1 order-md-1 order-lg-1 order-xl-1" >
             <div class="row h-100 align-items-end">
                 <div class="col-12 text-left text-center">
-                    <div class=" h4" style="color: whitesmoke"> Фотография это просто! Первые 5 снимков в первый день! </div>
+                    <div class=" h4" > Фотография это просто! Первые 5 снимков в первый день! </div>
                 </div>
             </div>
         </div>
@@ -73,7 +73,15 @@
         <p class="h1">dddddddddddd</p>
 
     </div>
-
+    <div class="row jumbotron paral paralsec3">
+        <div class="col-12 col-sm-6 order-last order-sm-1 order-md-1 order-lg-1 order-xl-1" >
+            <div class="row h-100 align-items-end">
+                <div class="col-12 text-left text-center">
+                    <div class=" h4"> Фотография это просто! Первые 5 снимков в первый день! </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <!--Main part-->
 
 
